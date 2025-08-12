72e7190814143a35b61eca56d2f139e3b14abf15
Pedro Silva <pedrosilva354@gmail.com>
2025-08-12 19:11:49 -0300

    Fix: things

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_LOCAL_URL,
 })
 
 async function query(queryString, params, callback) {
