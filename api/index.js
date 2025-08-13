420455bcd81b4193f5c96a13cd44b4a059f09f00
Pedro Silva <pedrosilva354@gmail.com>
2025-08-12 21:50:29 -0300

    update

--- api/index.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  // Sua lógica aqui
-  if (req.method === 'GET') {
-    res.json({ message: 'Hello from Vercel!' });
-  }
-}
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const dotenv = require('dotenv');
 dotenv.config();
+
 const express = require('express');
 const path = require('path');
+
 const session = require('express-session');
+
 const messageHandler = require('./middlewares/messageHandler');
 const pgSession = require('connect-pg-simple')(session);
-const router = require('./router');
-const logger  = require('./middlewares/logger');
+const router = require('./router/index');
+const logger = require('./middlewares/logger');
 const errorController = require('./controllers/errorController');
 const errorHandler = require('./middlewares/errorHandler');
 // instanciando o servidor
@@ -17,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'))
 // middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
     store: new pgSession({
@@ -42,7 +45,7 @@ app.use(errorController.notFound);
 app.use(errorHandler)
 // servidor
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>  {console.log(`Servidor http://localhost:${PORT}`)})
+app.listen(PORT, () => { console.log(`Servidor http://localhost:${PORT}`) })
 
 module.exports = app;
 
