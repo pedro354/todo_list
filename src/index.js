adba1b09a9629727344865b2a63746e811f4bf1c
Pedro Silva <pedrosilva354@gmail.com>
2025-08-14 16:19:48 -0300

    v3.5.7

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config();
+require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 const router = require('./routes');
@@ -7,26 +7,29 @@ const messageHandler = require('./middlewares/messageHandler');
 const logger = require('./middlewares/logger');
 const errorController = require('./controllers/errorController');
 const errorHandler = require('./middlewares/errorHandler');
+
 // instanciando o servidor
 const app = express();
-// configurações
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
-// middlewares
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
-app.use('/css', express.static(path.join(__dirname, 'public/css')));
-app.use('/script', express.static(path.join(__dirname, 'public/script')));
-// ✅ COOKIE SESSION (ainda melhor):
-const cookieSession = require('cookie-session');
 
+// configurações para o cors
 const vercelFrontEndUrl = "https://todo-list-2cfs.onrender.com";
 const corsOptions = {
     origin: [vercelFrontEndUrl, 'http://localhost:3000'],
-    Credential: true,
+    credentials: true,
 }
 app.use(cors(corsOptions));
 
+// configurações
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+// middlewares
+app.use(express.urlencoded({ extended: true }));
+// Servindo arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')))
+
+// configuração do cookie-session
+const cookieSession = require('cookie-session');
 app.use(cookieSession({
     name: 'session',
     keys: [process.env.SESSION_SECRET],
@@ -34,15 +37,15 @@ app.use(cookieSession({
 }));
 app.use(messageHandler);
 app.use(logger);
-// rotas principais
+
+// Rotas
 app.use(router);
-// tratamento de erros
+
+// Tratamento de erros
 app.use(errorController.notFound);
 app.use(errorHandler)
-// servidor
 
-
-const PORT = process.env.PORT && 3000 || process.env.RENDER_PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}/`);
 });
