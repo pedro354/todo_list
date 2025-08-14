08bb60b7e298a5bc5a892c4fefafd04ed93fd8f8
Pedro Silva <pedrosilva354@gmail.com>
2025-08-14 12:03:30 -0300

    v3.0

--- database/db.js
+++ database/db.js
@@ -3,6 +3,12 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    ssl: false
 })
 
 async function query(queryString, params, callback) {
