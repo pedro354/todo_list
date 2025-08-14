6e6679463715f2c747db3b6ea0abb129ebf61906
Pedro Silva <pedrosilva354@gmail.com>
2025-08-14 15:33:08 -0300

    v3.5.2

--- src/index.js
+++ src/index.js
@@ -33,7 +33,5 @@ app.use(errorHandler)
 // servidor
 
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 3000 && process.env.RENDER_PORT;
+console.log('Server is running on port', PORT);
