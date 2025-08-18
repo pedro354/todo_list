57a255968a2beae6cbc8327de69a7020dc78e149
Pedro Silva <pedrosilva354@gmail.com>
2025-08-18 19:11:00 -0300

    refatoração no vercel v.223

---
+++ public/script/utls.js
@@ -0,0 +1,20 @@
+// utls.js
+const dns = require("dns");
+
+function validationEmail(dominio) {
+  const domainProhibited = ["email.com", "teste.com", "example.com"];
+  if (domainProhibited.includes(dominio)) {
+    return false;
+  }
+  return new Promise((resolve, reject) => {
+    dns.resolveMx(dominio, (err, address) => {
+      if (err || !address || address.length === 0) {
+        reject({ error: "Email não encontrado" });
+      } else {
+        resolve({ message: "Email encontrado", domain: dominio });
+      }
+    });
+  });
+}
+
+module.exports = { validationEmail };
