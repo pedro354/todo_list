7e8b97ee53bc0f61bfeb9938508b96c2053ffba3
Pedro Silva <pedrosilva354@gmail.com>
2025-08-25 20:47:40 -0300

    refatoração no render

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // configurações
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views/'));
+app.set('views', path.join(__dirname, 'src/views'));
 // Servindo arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
