886a85cea7c8467edee1b75f65e79ca9a75ca2c2
Pedro Silva <pedrosilva354@gmail.com>
2025-10-22 11:11:52 -0300

    update todolist no supabase e koyeb

--- database/db.js
+++ database/db.js
@@ -2,17 +2,16 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: false
+});
 
 async function query(queryString, params, callback) {
-    return pool.query(queryString, params, callback);
-    
+  return pool.query(queryString, params, callback);
 }
 
 async function getClient() {
-    return pool.connect(); // retorna uma promise 
+  return pool.connect();
 }
 
-module.exports = {query, getClient}
+module.exports = { query, getClient };
