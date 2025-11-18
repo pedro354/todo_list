7bbb04aed293c2a80def15a8a4a325c252771ad8
Pedro Silva <pedrosilva354@gmail.com>
2025-11-18 19:51:15 -0300

    update todolist no render aaaaaaaaaaaaa

--- database/db.js
+++ database/db.js
@@ -1,19 +1,22 @@
+require('dotenv-flow').config();
 const { Pool } = require("pg");
 
+// Configuração geral
 const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: isProduction ? { rejectUnauthorized: false } : false
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
+// Query padrão
 async function query(text, params) {
-    return pool.query(text, params);
+  return pool.query(text, params);
 }
 
+// Cliente manual (transações)
 async function getClient() {
-    return pool.connect();
+  return pool.connect();
 }
 
-module.exports = { query, getClient, pool };
-
+module.exports = { query, getClient };
