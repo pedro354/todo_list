089d3814dbdf7be652a228f503d3df25e78710e4
Pedro Silva <pedrosilva354@gmail.com>
2025-11-18 21:20:36 -0300

    update todolist no render...

--- database/db.js
+++ database/db.js
@@ -5,7 +5,9 @@ const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Query padrão
